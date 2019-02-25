4b2370e7333c9ff1b3608ee5bfb2285b27e68e67
Vitaly Ogoltsov <vitaly.ogoltsov@me.com>
2019-02-26 01:20:20 +0300

    feat: Implemented animation panel for armature object in pose mode.

--- animation.py
+++ animation.py
@@ -19,6 +19,18 @@
 import bpy
 
 
+def property_get_bool(object: bpy.types.Object, property: str) -> bool:
+    return bool(object[property])
+
+
+def property_set_int(object: bpy.types.Object, property: str, value: bool):
+    object[property] = int(value)
+
+
+def property_set_float(object: bpy.types.Object, property: str, value: bool):
+    object[property] = float(value)
+
+
 class UE4_TOOLS_ANIMATION_prefs(bpy.types.PropertyGroup):
     """Animation tools preferences."""
     pass
@@ -62,6 +74,7 @@ class UE4_TOOLS_ANIMATION_PT_main(bpy.types.Panel):
         pass
 
     def __draw_armature_in_pose_mode(self, context: bpy.types.Context, active_object: bpy.types.Object):
+        # check if this is a compatible armature
         if 'DeformBones' not in active_object.pose.bone_groups:
             self.layout.label(text='Incompatible armature', icon='ERROR')
             self.layout.label(text='Selected armature must')
@@ -73,9 +86,163 @@ class UE4_TOOLS_ANIMATION_PT_main(bpy.types.Panel):
             self.layout.label(text='and hit the button below.')
             self.layout.operator(UE_TOOLS_ANIMATION_OT_set_deform_bones_group.bl_idname)
             pass
+
+        # visibility
+        self.layout.label(text='Visibility Options')
+        layout_visibility = self.layout.column()
+        layout_visibility_row = layout_visibility.row()
+        layout_visibility_row.use_property_split = False
+        layout_visibility_row.prop(active_object, 'show_in_front', text='X Ray', toggle=True, icon='XRAY')
+        layout_visibility_row.prop(active_object.data, 'show_names', text='Names', toggle=True, icon='TEXT')
+        layout_visibility_row.prop(active_object.data, 'show_axes', text='Axes', toggle=True, icon='EMPTY_AXIS')
+        layout_visibility_row = layout_visibility.row()
+        layout_visibility_row.use_property_split = False
+        layout_visibility_row.prop(active_object.data, 'layers', index=0, toggle=True, text='Deform', icon='BONE_DATA')
+        layout_visibility_row.prop(active_object.data, 'layers', index=2, toggle=True, text='Helper', icon='POSE_HLT')
+
+        # draw separator
+        self.layout.separator()
+
+        # constraints and ik
+        self.layout.label(text='Constraints and IK')
+        # toggle constraints on/off
+        self.prop_toggle(active_object, 'Constraints_ON_OFF', text='Enable Constraints', icon='CONSTRAINT')
+        # draw sublayout for ik
+        self.prop_toggle(active_object, 'IKMAIN', text='Enable IK', icon='LINKED', icon_off='UNLINKED')
+        layout_ik = self.layout.column()
+        layout_ik.enabled = property_get_bool(active_object, 'IKMAIN')
+        # draw sublayout for arms ik
+        layout_ik_arms_root = layout_ik.box()
+        self.prop_toggle(active_object, 'IKARMS', text='Enable Arms IK', layout=layout_ik_arms_root, icon='LINKED', icon_off='UNLINKED')
+        layout_ik_arms = layout_ik_arms_root.column()
+        layout_ik_arms.enabled = property_get_bool(active_object, 'IKARMS')
+        layout_ik_arms_row = layout_ik_arms.row()
+        layout_ik_arms_row.use_property_split = False
+        if not property_get_bool(active_object, 'ShowAdvancedProps'):
+            self.prop_toggle(active_object, 'Ik Arm R', text='Arm R', type='float', layout=layout_ik_arms_row, icon='LOCKED', icon_off='UNLOCKED')
+            self.prop_toggle(active_object, 'IK Arm L', text='Arm L', type='float', layout=layout_ik_arms_row, icon='LOCKED', icon_off='UNLOCKED')
         else:
-            self.layout.label(text='TODO', icon='SCRIPT')
-            pass
+            layout_ik_arms_row.prop(active_object, '["Ik Arm R"]', text='Arm R', slider=True)
+            layout_ik_arms_row.prop(active_object, '["IK Arm L"]', text='Arm L', slider=True)
+        layout_ik_arms_row = layout_ik_arms.row()
+        layout_ik_arms_row.use_property_split = False
+        if not property_get_bool(active_object, 'ShowAdvancedProps'):
+            self.prop_toggle(active_object, 'Ik hand R Lock', text='Hand R', type='float', layout=layout_ik_arms_row, icon='LOCKED', icon_off='UNLOCKED')
+            self.prop_toggle(active_object, 'Ik Hand L Lock', text='Hand L', type='float', layout=layout_ik_arms_row, icon='LOCKED', icon_off='UNLOCKED')
+        else:
+            layout_ik_arms_row.prop(active_object, '["Ik hand R Lock"]', text='Hand R', slider=True)
+            layout_ik_arms_row.prop(active_object, '["Ik Hand L Lock"]', text='Hand L', slider=True)
+        # draw sublayout for legs ik
+        layout_ik_legs_root = layout_ik.box()
+        self.prop_toggle(active_object, 'IKLEGS', text='Enable Legs IK', layout=layout_ik_legs_root, icon='LINKED', icon_off='UNLINKED')
+        layout_ik_legs = layout_ik_legs_root.column()
+        layout_ik_legs.enabled = property_get_bool(active_object, 'IKLEGS')
+        layout_ik_legs_row = layout_ik_legs.row()
+        layout_ik_legs_row.use_property_split = False
+        if not property_get_bool(active_object, 'ShowAdvancedProps'):
+            self.prop_toggle(active_object, 'Ik Leg R', text='Leg R', type='float', layout=layout_ik_legs_row, icon='LOCKED', icon_off='UNLOCKED')
+            self.prop_toggle(active_object, 'Ik Leg L', text='Leg L', type='float', layout=layout_ik_legs_row, icon='LOCKED', icon_off='UNLOCKED')
+        else:
+            layout_ik_legs_row.prop(active_object, '["Ik Leg R"]', text='Leg R', slider=True)
+            layout_ik_legs_row.prop(active_object, '["Ik Leg L"]', text='Leg L', slider=True)
+        layout_ik_legs_row = layout_ik_legs.row()
+        layout_ik_legs_row.use_property_split = False
+        if not property_get_bool(active_object, 'ShowAdvancedProps'):
+            self.prop_toggle(active_object, 'Foot Lock R', text='Foot R', type='float', layout=layout_ik_legs_row, icon='LOCKED', icon_off='UNLOCKED')
+            self.prop_toggle(active_object, 'Foot Lock L', text='Foot L', type='float', layout=layout_ik_legs_row, icon='LOCKED', icon_off='UNLOCKED')
+        else:
+            layout_ik_legs_row.prop(active_object, '["Foot Lock R"]', text='Foot R', slider=True)
+            layout_ik_legs_row.prop(active_object, '["Foot Lock L"]', text='Foot L', slider=True)
+
+        # rotation inheritance
+        self.layout.label(text='Inherit Rotation')
+        layout_rotation = self.layout.column()
+        layout_rotation.use_property_split = False
+        if not property_get_bool(active_object, 'ShowAdvancedProps'):
+            self.prop_toggle(active_object, 'Head inherit Rotation', text='Head', type='float', layout=layout_rotation, icon='LOCKED', icon_off='UNLOCKED')
+            self.prop_toggle(active_object, 'Arms inherit Rotation', text='Arms', type='float', layout=layout_rotation, icon='LOCKED', icon_off='UNLOCKED')
+            self.prop_toggle(active_object, 'Waist Inherit Rotation', text='Waist', type='float', layout=layout_rotation, icon='LOCKED', icon_off='UNLOCKED')
+        else:
+            layout_rotation.prop(active_object, '["Head inherit Rotation"]', text='Head', slider=True)
+            layout_rotation.prop(active_object, '["Arms inherit Rotation"]', text='Arms', slider=True)
+            layout_rotation.prop(active_object, '["Waist Inherit Rotation"]', text='Waist', slider=True)
+
+        # advanced view button
+        self.layout.separator()
+        self.prop_toggle(active_object, 'ShowAdvancedProps', text='Advanced View', icon='VISIBLE_IPO_ON', icon_off='VISIBLE_IPO_OFF')
+
+    def prop_toggle(self, object: bpy.types.Object, property: str, type: str = 'int',
+                    text: str = None, icon: str = 'NONE', icon_off=None,
+                    layout: bpy.types.UILayout = None):
+
+        operator: bpy.types.OperatorProperties
+        operator = (layout or self.layout).operator(UE_TOOLS_ANIMATION_OT_toggle_rig_property.bl_idname,
+                                                    text=text or property,
+                                                    emboss=True, depress=property_get_bool(object, property),
+                                                    icon=icon if property_get_bool(object, property) else (icon_off or icon))
+        operator.property = property
+        operator.type = type
+
+
+class UE_TOOLS_ANIMATION_OT_toggle_rig_property(bpy.types.Operator):
+    bl_idname = 'ue4_tools_animation.toggle_rig_property'
+    bl_label = 'Toggle Rig property'
+
+    property: bpy.props.StringProperty()
+    type: bpy.props.StringProperty()
+
+    def execute(self, context: bpy.types.Context):
+        active_object = context.view_layer.objects.active
+        # toggle property enabled state
+        property_enabled = property_get_bool(active_object, self.property)
+        property_enabled = not property_enabled
+        if self.type == 'int':
+            property_set_int(active_object, self.property, property_enabled)
+        elif self.type == 'float':
+            property_set_float(active_object, self.property, property_enabled)
+        else:
+            raise Exception('Unsupported toggle property type: %s' % self.type)
+        # special callbacks for certain properties
+        if self.property == 'IKMAIN':
+            self.__toggle_ikmain(active_object, property_enabled)
+        if self.property == 'IKARMS':
+            self.__toggle_ikarms(active_object, property_enabled)
+        if self.property == 'IKLEGS':
+            self.__toggle_iklegs(active_object, property_enabled)
+        return {'FINISHED'}
+
+    def __toggle_ikmain(self, active_object: bpy.types.Object, property_enabled: bool):
+        property_set_int(active_object, 'IKARMS', property_enabled)
+        property_set_int(active_object, 'IKLEGS', property_enabled)
+        self.__toggle_ikarms(active_object, property_enabled)
+        self.__toggle_iklegs(active_object, property_enabled)
+
+    def __toggle_ikarms(self, active_object: bpy.types.Object, property_enabled: bool):
+        property_set_float(active_object, 'Ik Hand L Lock', property_enabled)
+        property_set_float(active_object, 'Ik hand R Lock', property_enabled)
+        property_set_float(active_object, 'IK Arm L', property_enabled)
+        property_set_float(active_object, 'Ik Arm R', property_enabled)
+        self.__set_bone_group_visibility(active_object, 'Ik_Arm_controls', property_enabled)
+        self.__set_bone_group_visibility(active_object, 'roll_Arms_controls', property_enabled)
+
+    def __toggle_iklegs(self, active_object: bpy.types.Object, property_enabled: bool):
+        property_set_float(active_object, 'Foot Lock L', property_enabled)
+        property_set_float(active_object, 'Foot Lock R', property_enabled)
+        property_set_float(active_object, 'Ik Leg L', property_enabled)
+        property_set_float(active_object, 'Ik Leg R', property_enabled)
+        self.__set_bone_group_visibility(active_object, 'Ik_Leg_controls', property_enabled)
+        self.__set_bone_group_visibility(active_object, 'roll_Legs_controls', property_enabled)
+
+    def __set_bone_group_visibility(self, active_object: bpy.types.Object, bone_group_name: str, visible: bool):
+        """ Updates visibility for all bones that are assigned a specified group."""
+        bone_groups = active_object.pose.bone_groups
+        bone_group_index = next((i for i in range(len(bone_groups)) if bone_groups[i].name == bone_group_name), None)
+        if bone_group_index is None:
+            self.report({'ERROR'}, 'Could not find bone group: %s' % bone_group_name)
+            return
+        for bone in (bone for bone in active_object.pose.bones if bone.bone_group_index == bone_group_index):
+            # we can only set 'hide' property on EditBone
+            active_object.data.bones[bone.name].hide = not visible
 
 
 class UE4_TOOLS_ANIMATION_OT_add_ue4_rig(bpy.types.Operator):
@@ -198,7 +365,7 @@ class UE_TOOLS_ANIMATION_OT_add_deform_bones_group(bpy.types.Operator):
             bpy.ops.pose.group_assign(new_group_index)
             armature_object.pose.bone_groups[new_group_index].name = 'DeformBones'
         else:
-            self.report({'ERROR'}, "Any bones have vertex associated")
+            self.report({'ERROR'}, 'No bones have corresponding vertex groups')
         # restore layer visibility
         for n in range(0, len(armature_object.data.layers)):
             armature_object.data.layers[n] = visible_bone_layers[n]
